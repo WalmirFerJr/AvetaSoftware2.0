c43e69eab3bda4707ba4d47a4758dbd4a38bc4e0
Walmirferjr <walmir.jr.2004@usp.br>
2026-02-24 23:38:54 -0300

    chore: initialize spring boot project

---
+++ src/main/java/com/AvetaSoftware/Aveta/AvetaApplication.java
@@ -0,0 +1,13 @@
+package com.AvetaSoftware.Aveta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AvetaSoftware/Aveta/AvetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AvetaSoftware.Aveta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
